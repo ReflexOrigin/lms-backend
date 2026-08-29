2d493babb60b4d8f0a893900ed6296f49ab25989
Light of Inspiration <dcreyentes@gmail.com>
2026-08-29 17:20:53 +0600

    fix: add url config for strapi 5 proxy trust

--- dist/config/server.js
+++ dist/config/server.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', ''),
     proxy: true,
     app: {
         keys: env.array('APP_KEYS'),
