aefad276d3c350b51b7f200059b8b869ac21baa8
Light of Inspiration <dcreyentes@gmail.com>
2026-08-29 17:21:48 +0600

    fix: use strapi 5 specific proxy object syntax

--- dist/src/api/blog-post/routes/blog-post.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::blog-post.blog-post', {
-    config: {
-        update: { policies: [{ name: 'global::is-owner', config: { contentType: 'api::blog-post.blog-post', ownerField: 'author' } }] },
-        delete: { policies: [{ name: 'global::is-owner', config: { contentType: 'api::blog-post.blog-post', ownerField: 'author' } }] },
-    },
-});
--- dist/src/api/question/routes/question.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::question.question', {
-    config: {
-        update: { policies: [{ name: 'global::is-course-owner', config: { contentType: 'api::question.question', parentField: 'quiz', ownerField: 'instructor' } }] }, // Note: parent of question is quiz, parent of quiz is course. Actually cascading doesn't work 2 levels deep out of the box in our policy. But we can just use the controller to secure question creation/update.
-        delete: { policies: [{ name: 'global::is-course-owner', config: { contentType: 'api::question.question', parentField: 'quiz', ownerField: 'instructor' } }] },
-    },
-});
