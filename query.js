3abfd9ca4acf9253c38f376a757f67f0e0a3e6b5
Light of Inspiration <dcreyentes@gmail.com>
2026-08-29 16:32:46 +0600

    chore: remove redundant scratchpads and untrack sqlite db

--- query.js
+++
@@ -1,8 +0,0 @@
-const db = require('better-sqlite3')('.tmp/data.db');
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log('Tables:', tables.map(r=>r.name).join(', '));
-const coursesTable = tables.find(t => t.name.includes('course'));
-if (coursesTable) {
-    const courses = db.prepare(`SELECT * FROM ${coursesTable.name}`).all();
-    console.log('Courses:', courses);
-}
--- query2.js
+++
@@ -1,8 +0,0 @@
-const db = require('better-sqlite3')('E:/LMS/lms-backend/data.db');
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log('Tables:', tables.map(r=>r.name).join(', '));
-const coursesTable = tables.find(t => t.name.includes('course') && !t.name.includes('lnk'));
-if (coursesTable) {
-    const courses = db.prepare(`SELECT document_id, title, slug FROM ${coursesTable.name}`).all();
-    console.log('Courses:', courses);
-}
